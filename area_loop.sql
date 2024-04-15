d04b2367af3cbbb0a28618be47b81b375233e2f2
DonaJince <donamaleri@gmail.com>
2024-04-15 13:26:10 +0530

    updated

--- area_loop.sql
+++ area_loop.sql
@@ -3,8 +3,17 @@ declare
 	r int:=&radius;
 	pi float:=3.14;
 	area float;
+	i int;
 begin
-	area:=pi*r*r;
-	dbms_output.put_line('area is '|| area);
+	if(r>0) then
+		area:=pi;
+		for i in 1..2
+			loop
+				area:=area*r;
+			end loop;
+		dbms_output.put_line('area is '|| area);
+	else 
+		dbms_output.put_line('area cannot be found');
+	end if;
 end;
 /
